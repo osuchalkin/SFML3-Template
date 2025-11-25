0191291fffce1c92fb109145779d387af09d4604
Oleh Suchalkin <oleg.suchalkin@gmail.com>
2025-11-25 21:53:53 +0200

    update ResourceManager

--- ResourceManager.cpp
+++ ResourceManager.cpp
@@ -20,6 +20,13 @@ void ResourceManager::loadTexture(const std::string& id, const std::string& path
     mTextures[id] = std::move(texture);
 }
 
+void ResourceManager::loadSound(const std::string& id, const std::string& path) {
+    sf::SoundBuffer buffer;
+    if (!buffer.loadFromFile(path))
+        throw std::runtime_error("Failed to load texture: " + path);
+    mSounds[id] = std::move(buffer);
+}
+
 const sf::Font& ResourceManager::getFont(const std::string& id) const {
     auto it = mFonts.find(id);
     if (it == mFonts.end())
@@ -31,5 +38,12 @@ const sf::Texture& ResourceManager::getTexture(const std::string& id) const {
     auto it = mTextures.find(id);
     if (it == mTextures.end())
         throw std::runtime_error("Texture not found: " + id);
+    return it->second;
+}
+
+const sf::SoundBuffer& ResourceManager::getSound(const std::string& id) const {
+    auto it = mSounds.find(id);
+    if (it == mSounds.end())
+        throw std::runtime_error("Sound not found: " + id);
     return it->second;
 }
--- ResourceManager.h
+++ ResourceManager.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 #include <map>
 #include <string>
 
@@ -10,10 +11,12 @@ public:
     // Загрузка
     void loadFont(const std::string& id, const std::string& path);
     void loadTexture(const std::string& id, const std::string& path);
+    void loadSound(const std::string& id, const std::string& path);
 
     // Получение
     const sf::Font& getFont(const std::string& id) const;
     const sf::Texture& getTexture(const std::string& id) const;
+    const sf::SoundBuffer& getSound(const std::string& id) const;
 
 private:
     ResourceManager() = default;
@@ -22,4 +25,5 @@ private:
 
     std::map<std::string, sf::Font> mFonts;
     std::map<std::string, sf::Texture> mTextures;
+    std::map<std::string, sf::SoundBuffer> mSounds;
 };
